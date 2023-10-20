45f12975e88958724ac40037362a69f86241f172
Limuran <strelgl2@gmail.com>
2023-10-20 18:04:09 +1000

    Add checks for int overflow and wrong split

--- src/test/java/edu/hw1/Task1Test.java
+++ src/test/java/edu/hw1/Task1Test.java
@@ -36,6 +36,26 @@ public class Task1Test {
     }
 
     @Test
+    @DisplayName("35791393:00")
+    void validValues_4() {
+        String time = "35791393:00";
+
+        int seconds = Task1.minutesToSeconds(time);
+
+        assertThat(seconds).isEqualTo(2147483580);
+    }
+
+    @Test
+    @DisplayName("35791393:59")
+    void validValues_5() {
+        String time = "35791393:59";
+
+        int seconds = Task1.minutesToSeconds(time);
+
+        assertThat(seconds).isEqualTo(2147483639);
+    }
+
+    @Test
     @DisplayName("10:60")
     void nonValidValues_1() {
         String time = "10:60";
@@ -59,6 +79,96 @@ public class Task1Test {
     @DisplayName("6:-05")
     void nonValidValues_3() {
         String time = "6:-05";
+
+        int seconds = Task1.minutesToSeconds(time);
+
+        assertThat(seconds).isEqualTo(-1);
+    }
+
+    @Test
+    @DisplayName("6:-5")
+    void nonValidValues_4() {
+        String time = "6:-5";
+
+        int seconds = Task1.minutesToSeconds(time);
+
+        assertThat(seconds).isEqualTo(-1);
+    }
+
+    @Test
+    @DisplayName("99999999999999999:00")
+    void nonValidValues_5() {
+        String time = "99999999999999999:00";
+
+        int seconds = Task1.minutesToSeconds(time);
+
+        assertThat(seconds).isEqualTo(-1);
+    }
+
+    @Test
+    @DisplayName("5dfdfdf9:30")
+    void nonValidValues_6() {
+        String time = "5dfdfdf9:30";
+
+        int seconds = Task1.minutesToSeconds(time);
+
+        assertThat(seconds).isEqualTo(-1);
+    }
+
+    @Test
+    @DisplayName("10:a3")
+    void nonValidValues_7() {
+        String time = "10:a3";
+
+        int seconds = Task1.minutesToSeconds(time);
+
+        assertThat(seconds).isEqualTo(-1);
+    }
+
+    @Test
+    @DisplayName("10:3a")
+    void nonValidValues_8() {
+        String time = "10:3a";
+
+        int seconds = Task1.minutesToSeconds(time);
+
+        assertThat(seconds).isEqualTo(-1);
+    }
+
+    @Test
+    @DisplayName("10:10:10")
+    void nonValidValues_9() {
+        String time = "10:10:10";
+
+        int seconds = Task1.minutesToSeconds(time);
+
+        assertThat(seconds).isEqualTo(-1);
+    }
+
+    @Test
+    @DisplayName("1010")
+    void nonValidValues_10() {
+        String time = "1010";
+
+        int seconds = Task1.minutesToSeconds(time);
+
+        assertThat(seconds).isEqualTo(-1);
+    }
+
+    @Test
+    @DisplayName("2147483647:00")
+    void nonValidValues_11() {
+        String time = "2147483647:12";
+
+        int seconds = Task1.minutesToSeconds(time);
+
+        assertThat(seconds).isEqualTo(-1);
+    }
+
+    @Test
+    @DisplayName("35791394:00")
+    void nonValidValues_12() {
+        String time = "35791394:00";
 
         int seconds = Task1.minutesToSeconds(time);
 
